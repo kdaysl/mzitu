1368f4e1808adef80f167d66cbc21a380435f541
kdaysl <kdaysl@outlook.com>
2019-10-18 20:55:01 +0800

    new

---
+++ mzitu.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*- 
+# Created by kdaysl in 2019/10/17 23:37 
+# @Site :  
+# @File : mzitu.py 
+# @Software: PyCharm
+
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+import os
+import random
+url = 'https://www.mzitu.com/'
+urls = 'https://www.mzitu.com/page/' #
+cur_path = os.getcwd() + '/爬虫库/'
+UA = [
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+    'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+    'Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+    'Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+    'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+    'MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile ',
+    'Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10',
+    'Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13',
+    'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+',
+    'Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0',
+    'Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-1/20.0.019; Profile/MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/525 (KHTML, like Gecko) ',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; ) Opera/UCWEB7.0.2.37/28/999',
+]
+headers = {
+
+        'User-Agent': choice(UA)
+    }
+proxy = {
+    "http": "http://{}:{}".format("217.219.93.77", "80"),
+    "https": "https://{}:{}".format("217.219.93.77", "80")
+}
+header = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36',
+    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate",
+    "Referer": "http://www.mzitu.com/",
+    'Cookie': 'bdshare_firstime=1504530621757; safedog-flow-item=',
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Cache-Control": "max-age=0"
+}
+
+for page in range(1,10):
+    print('page:' + img_urls + '开始')
+    cur_url = urls + str(page)
+    cur_page = requests.get(cur_url, headers=headers)
+    cur_page.encoding = 'utf-8'
+    soup = BeautifulSoup(cur_page.text, 'html.parser')
+    img_i = soup.find(id='pins').find_all('a')[1::2]
+    print(img_i)
+    index = 1
+    for img_urls in img_i:
+        img_url = img_urls.attrs["href"]
+        dir_name = img_urls.get_text().strip().replace('?', '')
+        pic_path = cur_path + dir_name
+        if os.path.exists(pic_path):
+            print('数据重复')
+        else:
+            os.mkdir(pic_path)
+            os.chdir(pic_path)
+            for index in range(1, 70):
+                url = img_url + '/' + str(index)
+                try:
+                    sqlite3.time.sleep(random.randint(0, 2))
+                    img_page = requests.get(url, headers=headers)
+                    img_page.encoding = 'utf-8'
+                    soup_url = BeautifulSoup(img_page.text, 'html.parser')
+                    img_src = soup_url.find('div', 'main-image').find('img')['src']
+                    img_name = img_src.split('/')[-1]
+                    print(dir_name + img_name + '   ' + img_src)
+                    f = open(img_name, 'wb')
+                    f.write(requests.get(img_src, headers=header).content)
+                    f.close()
+                    os.chdir(pic_path)
+                except:
+                    break
+                    print('error')
+                finally:
+                    print('page:'+index+"  page over")
+
+
+#imgsrcs=soup.find_all('img')
+
+
+
+
+
+
+
+
+
